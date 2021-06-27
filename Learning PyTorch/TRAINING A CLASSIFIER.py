679dd9bc2bf2c1e9ab3e64c7f316652550c99513
Athiq <athiqahmed.ai@gmail.com>
2021-06-27 22:24:59 +0530

    updating the file

--- Learning PyTorch/TRAINING A CLASSIFIER.py
+++ Learning PyTorch/TRAINING A CLASSIFIER.py
@@ -1,12 +1,120 @@
 """
-Load and normalize the CIFAR10 training and test datasets using torchvision
-Define a Convolutional Neural Network
-Define a loss function
-Train the network on the training data
-Test the network on the test data
+1. Load and normalize the CIFAR10 training and test datasets using torchvision
+2. Define a Convolutional Neural Network
+3. Define a loss function
+4. Train the network on the training data
+5. Test the network on the test data
 """
 
+# 1. Load and normalize CIFAR10
 import torch
 import torchvision
 import torchvision.transforms as transforms
 
+# The output of torchvision datasets are PILImage images of range [0, 1]. We transform them to Tensors of normalized range [-1, 1]
+transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+print(transform)
+
+batch_size = 4
+
+train_dataset = torchvision.datasets.CIFAR10(root='./data',
+                                             download=True,
+                                             train=True,
+                                             transform=transform)
+
+test_dataset = torchvision.datasets.CIFAR10(root='./data',
+                                            download=True,
+                                            train=False,
+                                            transform=transform)
+
+train_dataloader = torch.utils.data.DataLoader(train_dataset,
+                                               batch_size=batch_size,
+                                               shuffle=True,
+                                               num_workers=2)
+
+test_dataloader = torch.utils.data.DataLoader(test_dataset,
+                                              batch_size=batch_size,
+                                              shuffle=False,
+                                              num_workers=2)
+
+classes = ('plane', 'car', 'bird', 'cat',
+           'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+# function to show img
+def imshow(img):
+    img = img / 2 + 0.5
+    npimg = img.numpy()
+    plt.imshow(np.transpose(npimg, (1,2,0)))
+    plt.show()
+
+# get some random training images
+dataiter = iter(train_dataloader)
+images, labels = dataiter.next()
+
+# show images
+imshow(torchvision.utils.make_grid(images))
+print(' '.join('%5s' %classes[labels[j]] for j in range(batch_size)))
+
+
+# 2. Define a Convolutional Neural Network
+import torch.nn as nn
+import torch.nn.functional as F
+
+class Net(nn.Module):
+    def __init__(self):
+        super(Net, self).__init__()
+        self.conv1 = nn.Conv2d(3, 6, 5) # 3 input image channel, 6 output channels, 5x5 square convolution kernel
+        self.conv2 = nn.Conv2d(6, 16, 5)
+        self.fc1 = nn.Linear(16*5*5, 120)
+        self.fc2 = nn.Linear(120, 84)
+        self.fc3 = nn.Linear(84, 10)
+
+    def forward(self, x):
+        x = F.max_pool2d(F.relu(self.conv1(x)), (2,2))  # Max pooling over a (2,2) window
+        x = F.max_pool2d(F.relu(self.conv2(x)), 2)  # if the size is a square, you can specify with a single number
+        x = torch.flatten(x, 1)  # flatten all dimensions except the batch dimension
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)
+        return x
+
+net = Net()
+print(net)
+
+
+# 3. Define a Loss function and optimizer
+import torch.optim as optim
+
+loss_fn = nn.CrossEntropyLoss()
+optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
+
+# 4. Train the network
+for epoch in range(2):
+    running_loss = 0.0
+    for i, data in enumerate(train_dataloader, 0):
+        inputs, labels = data  # get the inputs; data is a list of [inputs, labels]
+
+        optimizer.zero_grad()  # zero the parameter gradients
+
+        # forward + backward + optimize
+        outputs = net(inputs)
+        loss = loss_fn(outputs, labels)
+        loss.backward()
+        optimizer.step()
+
+        # print statistics
+        running_loss += loss.item()
+        if i % 2000 == 1999: # print every 2000 mini batch
+            print(f'{epoch+1, i+1}, loss: {running_loss/2000}')
+            running_loss = 0.0
+
+print('Finished Training')
+
+# Save the model
+PATH = './cifar_net.pth'
+torch.save(net.state_dict(), PATH)
+
+# 5. Test the network on the test data
