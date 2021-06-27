396088e8366e578755661ea1fd7e02fb0ff6c065
Athiq <athiqahmed.ai@gmail.com>
2021-06-27 21:41:33 +0530

    adding the task

--- Learning PyTorch/TRAINING A CLASSIFIER.py
+++ Learning PyTorch/TRAINING A CLASSIFIER.py
@@ -1,3 +1,12 @@
+"""
+Load and normalize the CIFAR10 training and test datasets using torchvision
+Define a Convolutional Neural Network
+Define a loss function
+Train the network on the training data
+Test the network on the test data
+"""
+
 import torch
 import torchvision
 import torchvision.transforms as transforms
+
